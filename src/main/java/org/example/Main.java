bce8adddeebb5f2dce7f12b4164eddd51f959d71
TOKATL1 <120321214+TOKATL1@users.noreply.github.com>
2022-12-17 09:54:09 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // for
+
+        for (int i = 1; i < 10; i++) {
+
+            System.out.println(i);
+
+
+        }
+        System.out.println("for döngusu biddi");
+
+        int i = 1;
+        //while
+
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+
+        }
+
+        System.out.println("while dongusu biddi");
+
+
+        //Do-While
+
+        int j = 1;
+        do {
+
+
+            System.out.println(j);
+
+            j += 2;
+
+        } while (j < 10);
+
+        System.out.println("Do-While dongusu bidti");
+
+
+    }
+
+
+}
